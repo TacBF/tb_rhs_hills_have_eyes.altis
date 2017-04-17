068e8abf775b3cf5b2cd12cbb653f0bfa6db2471
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:41:21 +0200

    tb_rhs_hills_have_eyes.altis mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,21 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<*> Help out your team by deploying <FO> Forward Outposts, you can have up to 6 constructed<br />
+<br />
+
+<titleFont>Mission specifc:</titleFont><br />
+<*> <green>Sniper</green> kits are only available for squads larger than 6 players<br />
+<*> <green>Heavy Anti Tank</green> kits are only available for squads larger than 6 players, 1 per 10 players<br />
+<br />
+
+<titleFont>Strategies and tips:</titleFont><br />
+<*> Fortify your points with available construction supplies<br />
+<*> Hide your <FO> FOs as best as you can to prevent it being camped by enemy armor<br />
+<*> Try to secure <yellow>Factory</yellow> objective as soon as possible<br />
+<*> Mines work well in areas where travel options are limited<br />
+<*> Heavy weaponry and construction supplies are available from FO crates, use them wisely<br />
+<*> Use your M133 GMG and/or M2A3 to supress enemy before advancing to next objective<br />
+<br />
+
